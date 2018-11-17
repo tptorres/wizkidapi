f0b629f1dadd1f4fb85a0f174590ca9b200f90f6
Tyler <tptorres5@gmail.com>
2018-11-17 09:10:54 -0500

    3rd

--- django_rest/wsgi.py
+++ django_rest/wsgi.py
@@ -2,6 +2,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest.settings')
 
