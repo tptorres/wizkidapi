bcb88de1b6bf9cc476c4b7dcaf4ade80e5e61b5b
Tyler <tptorres5@gmail.com>
2018-11-17 09:17:57 -0500

    4th

--- django_rest/wsgi.py
+++ django_rest/wsgi.py
@@ -2,10 +2,10 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
 
