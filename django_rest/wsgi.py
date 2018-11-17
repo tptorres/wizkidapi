229a83035960244f18fdc174d7f8ec00874afa84
Tyler <tptorres5@gmail.com>
2018-11-17 09:06:47 -0500

    2nd

--- django_rest/wsgi.py
+++ django_rest/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for django_rest project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,3 +6,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest.settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
